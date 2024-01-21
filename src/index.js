4d678df91a8f9aeaffc084390806982466ce8777
animeshryu <animeshryu@gmail.com>
2024-01-22 03:20:08 +0530

    new

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Hero from './components/Hero';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Hero/>
+    <SpeedInsights />
   </React.StrictMode>
 );
