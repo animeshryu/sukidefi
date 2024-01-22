e45d89926e2cb094c165ca79fed8d851d40f4df1
animeshryu <animeshryu@gmail.com>
2024-01-22 15:05:05 +0530

    new

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -26,17 +26,16 @@ export default function Example() {
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pr-8 lg:pt-4">
             <div className="lg:max-w-lg">
-              <h2 className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
-              <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">A better workflow</p>
-              <p className="mt-6 text-lg leading-8 text-gray-600">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
-                iste dolor cupiditate blanditiis ratione.
+              <h2 className="text-base font-semibold leading-7 text-white">Grow Faster</h2>
+              <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">At your service</p>
+              <p className="mt-6 text-lg leading-8 text-white">
+                
               </p>
-              <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
+              <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-white lg:max-w-none">
                 {features.map((feature) => (
                   <div key={feature.name} className="relative pl-9">
-                    <dt className="inline font-semibold text-gray-900">
-                      <feature.icon className="absolute left-1 top-1 h-5 w-5 text-indigo-600" aria-hidden="true" />
+                    <dt className="inline font-semibold text-white">
+                      <feature.icon className="absolute left-1 top-1 h-5 w-5 text-white" aria-hidden="true" />
                       {feature.name}
                     </dt>{' '}
                     <dd className="inline">{feature.description}</dd>
